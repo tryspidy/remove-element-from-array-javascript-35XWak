785a25c2c70af155d6539a8b72c7b90882bb9893
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 13:09:34 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let fruit = ['apple', 'banana', 'orange', 'lettuce']; 
+// ^^ An example array that needs to have one item removed
+
+fruit.splice(3, 1); // Removes an item in the array using splice() method
+// First argument is the index of removal
+// Second argument is the amount of items to remove from that index and on
+
